32c816a47c0d15cb2966677168e6f86eff06123b
joshua graham <joshscottgraham@gmail.com>
2020-10-28 12:17:32 +0000

    Added more hashing functionality and command line interface
    todo: add comments

--- HashCracker/Cracker.py
+++ HashCracker/Cracker.py
@@ -1,37 +1,85 @@
 import crypt
 import hashlib
-def MD5crack(encryptedPassword):
+import argparse
+def printOutput(passwordFound, passwordCracked, username):
+    if(passwordFound):
+        print("[+] Password Found: "+username + ":"+passwordCracked)
+    else:
+        print("[ - ] No password found for "+username)
+
+def MD5crack(encryptedPassword, wordlist, username):
     salt = encryptedPassword[0:2]
-    passwordFile = open('password.txt', 'r')
+    passwordFile = open(wordlist, 'r')
     for password in passwordFile.readlines():
         password = password.strip('\n')
         encryptedWord = crypt.crypt(password,salt)
         if (encryptedWord == encryptedPassword):
-            print("[+] Password Found: "+password +"\n")
+            printOutput(True, encryptedWord, username)
             return
-    print("[ - ] Password not found \n")
-    return
+    printOutput(False, encryptedPassword, username)
 
-def SHA512Crack(encryptedPassword):
+def SHA512crack(encryptedPassword, wordlist, username):
     salt = encryptedPassword[0:2]
-    passwordFile = open('password.txt','r')
+    passwordFile = open(wordlist,'r')
     for password in passwordFile.readlines():
         password = password.strip('\n')
         encryptedWord = hashlib.sha512(password, salt)
         encryptedWord.hexdigest()
         if (encryptedWord == encryptedPassword):
-            print("[+} Password Found: "+password+"\n")
+            printOutput(True, encryptedWord, username)
             return
-    print("[ - ] Password not found \n")
+    printOutput(False, encryptedPassword, username)
+
+def SHA1crack(encryptedPassword, wordlist, username):
+    salt = encryptedPassword[0:2]
+    passwordFile = open(wordlist,'r')
+    for password in passwordFile.readlines():
+        password = password.strip('\n')
+        encryptedWord = hashlib.sha1(password, salt)
+        encryptedWord.hexdigest()
+        if (encryptedWord == encryptedPassword):
+            printOutput(True, encryptedWord, username)
+            return
+    printOutput(False, encryptedPassword, username)
+    
+def SHA256crack(encryptedPassword, wordlist, username):
+    salt = encryptedPassword[0:2]
+    passwordFile = open(wordlist,'r')
+    for password in passwordFile.readlines():
+        password = password.strip('\n')
+        encryptedWord = hashlib.sha256(password, salt)
+        encryptedWord.hexdigest()
+        if (encryptedWord == encryptedPassword):
+            printOutput(True, encryptedWord, username)
+            return
+    printOutput(False, encryptedPassword, username)
+
+
+def readFile(hashFile, wordlist, algorithm):
+
+    passFile = open(hashFile, 'r')
+    for line in passFile.readlines():
+            if ":" in line:
+                user = line.split(':')[0]
+                encryptedPassword = line.split(':')[1].strip(' ')
+                print("[*] Cracking Password Hash for: "+user)
+                if(algorithm == ""):
+                     MD5crack(encryptedPassword, wordlist, user)
+                elif (algorithm == "sha1"):
+                    SHA1crack(encryptedPassword, wordlist, user)
+                elif (algorithm == "sha256"):
+                    SHA256crack(encryptedPassword, wordlist, user)
+                elif (algorithm == "sha512"):
+                    SHA512crack(encryptedPassword, wordlist, user)
 
 def main():
-    passFile = open('crack.txt')
-    for line in passFile.readlines():
-        if ":" in line:
-            user = line.split(':')[0]
-            encryptedPassword = line.split(':')[1].strip(' ')
-            print("[*] Cracking Password Hash for: "+user)
-            MD5crack(encryptedPassword)
+
+    parser = argparse.ArgumentParser(description="Crack Hashses")
+    parser.add_argument("-h", "--hashlist", metavar="HashList", type=str, required=True, help="Hashfile containing hashes to be cracked with their salts")
+    parser.add_argument("-w", "--wordlist", metavar="WordList", type=str, required=True, help="Word list for cracking the hash")
+    parser.add_argument("-c", "--crack", metavar="Crack", type=str, required=False, help="Specify what hashing algorithm is to be used")
+    args = parser.parse_args()
+    readFile(args.hashlist,args.wordlist, args.crack)
+
 if __name__ == "__main__":
     main()
-
